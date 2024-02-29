3513dccc245866a53705fdefd4fca0a55693c293
Ahmed Rayyan <ahmedk.ryan@gmail.com>
2024-02-29 17:08:59 +0200

    Implemented Signup & Login Endpoint
    #TODO : Session Management & Cookies

--- routes/api/login/index.js
+++ routes/api/login/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 
-
-router.post('/')
+router.post('/', async (req, res) => { 
+    console.log(req.body);
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email,
+        }
+    });
+    
+    if (user != null && user.password === password) {
+        res.json({ message: 'Logged In' });
+        return;
+    }
+   
+    else {
+        res.json({ message : 'Invalid Email Or Password' });
+    }
+}
+)
 
 
 module.exports = router;
--- routes/api/signup/index.js
+++ routes/api/signup/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 
 
-router.post('/')
+router.post('/', async (req, res) => {
+
+    if (req.body.email && req.body.password && req.body.name) {
+        
+        prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }}).then((user) => {
+            res.json({ user: user, success: true });
+        }).catch((err) => {
+            res.json({ success: false });
+            console.log(err);
+        });
+        
+    }
+    
+    else {
+        res.json({ res: "Please Provide Your Name, Email & Password to Signup" });
+    }
+});
 
 
 module.exports = router;
