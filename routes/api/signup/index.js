9c56a7242f6c6a160e50b163a93141794e2518cf
Ahmed Rayyan <ahmedk.ryan@gmail.com>
2024-03-01 17:55:08 +0200

    Checks if The User is Already Signed Up Before

--- routes/api/signup/index.js
+++ routes/api/signup/index.js
@@ -11,19 +11,35 @@ router.post('/', async (req, res) => {
 
     if (req.body.email && req.body.password && req.body.name) {
 
-        bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
-           await prisma.user.create({
-                data: {
-                    name: req.body.name,
-                    email: req.body.email,
-                    password: hash
-                }}).then((user) => {
-                res.json({ user: user, success: true });
-            }).catch((err) => {
-                res.json({ success: false });
-                console.log(err);
-            });
+        await prisma.user.findUnique({
+            where: {
+                email: req.body.email
+            }
+        }).then((user) => {
+            if (user) {
+                res.json({ res: "User Already Exists, You Can Sign in" });
+            }
+            else {
+                bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+                    await prisma.user.create({
+                        data: {
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash
+                        }
+                    }).then((user) => {
+                        res.json({ user: user, success: true });
+                    }).catch((err) => {
+                        res.json({ success: false });
+                        console.log(err);
+                    });
+                });
+            }
+        }).catch((err) => {
+            console.log(err);
         });
+
+
         
 
         
