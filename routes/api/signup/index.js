70dce34bdbcc32b84b4f40c1632a4d618e1e3bd5
Ahmed Rayyan <ahmedk.ryan@gmail.com>
2024-03-01 17:49:30 +0200

    Add bncrypt to hash Passwords Before Adding to DB

--- routes/api/signup/index.js
+++ routes/api/signup/index.js
@@ -2,24 +2,30 @@ const express = require("express");
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 
 router.post('/', async (req, res) => {
 
     if (req.body.email && req.body.password && req.body.name) {
-        
-        prisma.user.create({
-            data: {
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            }}).then((user) => {
-            res.json({ user: user, success: true });
-        }).catch((err) => {
-            res.json({ success: false });
-            console.log(err);
+
+        bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+           await prisma.user.create({
+                data: {
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash
+                }}).then((user) => {
+                res.json({ user: user, success: true });
+            }).catch((err) => {
+                res.json({ success: false });
+                console.log(err);
+            });
         });
+        
+
         
     }
     
