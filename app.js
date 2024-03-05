2dfe6c3ec2f24591ab385617de9e9783c3d45244
Ahmed Rayyan <ahmedk.ryan@gmail.com>
2024-03-05 13:11:31 +0200

    Moder Server Initialization into Server.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
-const PORT = process.env.PORT || 3000;
 
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(("*"),express.urlencoded({ extended: true }));
@@ -16,9 +15,4 @@ app.use("/api", require("./routes/api"));
 // 404 Not Found
 app.get("*", (req, res) => {
   res.status(404).send("Not Found");
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server Working On Port ${PORT}\n`);
 });
